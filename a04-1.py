7b4d589ed2952418c45ab6e01e28f4316d82180e
Tomek Kobylinski <tomasz.kobylinski@silksh.pl>
2021-12-05 01:35:21 +0800

    a04

--- a04-1.py
+++ a04-1.py
@@ -1,56 +1,66 @@
 import re
 from functools import reduce
 from itertools import chain
+from types import SimpleNamespace
 
-if __name__ == '__main__':
+
+def read_puzzles_from_file():
     file = open('a04-data.txt', 'r')
     lines = file.readlines()
     bingo_numbers = [int(num) for num in lines[0].strip().split(",")]
-    puzzle_num = (len(lines) - 1) / 6
-    if not puzzle_num.is_integer():
+    number_of_puzzles = (len(lines) - 1) / 6
+    if not number_of_puzzles.is_integer():
         raise ValueError
-    puzzle_num = int(puzzle_num)
-
-    row = 2
+    number_of_puzzles = int(number_of_puzzles)
+    file_row = 2
     puzzles = []
-    for i in range(0, puzzle_num):
+    for i in range(0, number_of_puzzles):
         puzzle = []
-        for j in range(0, 5):
-            puzzle.append([(int(num), False) for num in re.split(r"\s+", lines[row].strip())])
-            row = row + 1
+        for row in range(0, 5):
+            puzzle.append([(int(num), False) for num in re.split(r"\s+", lines[file_row].strip())])
+            file_row = file_row + 1
         puzzles.append(puzzle)
-        row = row + 1
+        file_row = file_row + 1
+    return SimpleNamespace(bingo_numbers=bingo_numbers, puzzles=puzzles, number_of_puzzles=number_of_puzzles)
 
 
-    def mark_number(bingo_number, puzzle_number):
-        for j in range(0, 5):
-            puzzles[puzzle_number][j] = list(
-                map(lambda x: (x[0], True) if x[0] == bingo_number else x, puzzles[puzzle_number][j]))
+def mark_number(puzzle, number):
+    for row in range(0, 5):
+        puzzle[row] = list(
+            map(lambda x: (x[0], True) if x[0] == number else x, puzzle[row]))
 
 
-    def check_bingo(puzzle_number):
-        rotated_puzzle = list(zip(*puzzles[puzzle_number][::-1]))
-        for j in range(0, 5):
-            row_bingo = reduce(lambda sum, element: sum if (element[1] is False) else sum + 1,
-                               puzzles[puzzle_number][j], 0)
-            if row_bingo == 5:
-                return True
+def check_bingo(puzzle):
+    def count_marked_in_row(row):
+        return reduce(lambda sum, element: sum if (element[1] is False) else sum + 1, row, 0)
 
-            column_bingo = reduce(lambda sum, element: sum if (element[1] is False) else sum + 1, rotated_puzzle[j], 0)
-            if column_bingo == 5:
-                return True
-
-        return False
-
-
-    def sum_unmarked(puzzle_number):
-        flattened_puzzle = chain.from_iterable(puzzles[puzzle_number])
-        return reduce(lambda sum, element: sum if (element[1] is True) else sum + element[0], flattened_puzzle, 0)
+    rotated_puzzle = list(zip(*puzzle[::-1]))
+    for row in range(0, 5):
+        row_bingo = count_marked_in_row(puzzle[row])
+        if row_bingo == 5:
+            return True
+        column_bingo = count_marked_in_row(rotated_puzzle[row])
+        if column_bingo == 5:
+            return True
+    return False
 
 
-    for bingo_number in bingo_numbers:
-        for i in range(0, puzzle_num):
-            mark_number(bingo_number, i)
-            if check_bingo(i) is True:
-                print(bingo_number * sum_unmarked(i))
+def sum_unmarked(puzzle):
+    flattened_puzzle = chain.from_iterable(puzzle)
+    return reduce(lambda sum, element: sum if (element[1] is True) else sum + element[0], flattened_puzzle, 0)
+
+
+def main():
+    data = read_puzzles_from_file()
+
+    for bingo_number in data.bingo_numbers:
+        for i in range(0, data.number_of_puzzles):
+            puzzle = data.puzzles[i]
+            mark_number(puzzle, bingo_number)
+            if check_bingo(puzzle) is True:
+                print("num:", bingo_number, "board:", i, "result:", bingo_number * sum_unmarked(puzzle))
                 quit()
+
+
+if __name__ == '__main__':
+    main()
